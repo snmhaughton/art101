a2ddc8b1ad5201a8da5e43503473b6e5fcdddd71
snmhaughton <77122583+snmhaughton@users.noreply.github.com>
2021-01-27 22:20:49 -0800

    index.html update

---
+++ lab4/js/lab.js
@@ -0,0 +1,8 @@
+// Shuffle and deal cards
+  //Leave rest of cards in middle of playing area
+    // Evaluate possibilities (which card user wants)
+    // Ask about certain cards (accept user input)
+      // Validate user move - If they do have card, take it
+      // If they don’t, draw from middle pile
+    // Choose computer move
+  // Test for win condition
---
+++ lab5/js/lab.js
@@ -0,0 +1,21 @@
+/*
+ * Author:  Noelle M-H <smillerh@ucsc.edu>
+ * Created: 1.27.2021
+ * License: Public Domain
+ */
+
+// Define Variables
+make = "Honda";
+model = "Accord";
+color = "Red";
+year = "1995";
+
+// calculate
+age = 2021 - year;
+
+// output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
