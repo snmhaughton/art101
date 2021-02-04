a89adf38d913198299547d3e9969988175549ba1
snmhaughton <77122583+snmhaughton@users.noreply.github.com>
2021-02-03 22:49:46 -0800

    js

---
+++ lab7/js/lab7.js
@@ -0,0 +1,26 @@
+/*
+ * Author:  Noelle M-H <smillerh@ucsc.edu>
+ * Created: 1.27.2021
+ * License: Public Domain
+ */
+
+//name sorter function and variable declared to sort names
+function nameSorter() {
+    var userName = window.prompt("Hello! Please enter your name.");
+    console.log("userName =", userName);
+    //turn string to array to sort
+    var nameArray = userName.split('');
+    console.log("nameArray =", nameArray);
+    //sort the nameArray
+    var nameArraySort = nameArray.sort();
+    console.log("nameArraySort =", nameArraySort);
+    //put array back with string
+    var nameSorted = nameArraySort.join('')
+    console.log("nameSorted =", nameSorted);
+    return nameSorted;
+
+}
+
+//output
+document.writeln("Here's your new name!",
+  sortUserName(), "</br>");
