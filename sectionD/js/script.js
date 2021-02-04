d0df3a09255561d3c4c13a78aaeba43bcdbb1657
snmhaughton <77122583+snmhaughton@users.noreply.github.com>
2021-02-03 21:38:44 -0800

    lab 7

--- sectionD/js/script.js
+++ sectionD/js/script.js
@@ -1,6 +1,29 @@
 console.log("javascript loaded");
 var nameVar = "Noelle";
 var numVar = 5;
-console.log(numVar);
+
 numVar = 25;
-console.log(numVar):
+
+
+function myFirstFunction (){
+  console.log("this is my first function!!");
+}
+
+//myFirstFunction();
+
+function comparingNumbers(firstNum, secondNum){
+  console.log("First Number: "+firstNum+" second Number: "+secondNum);
+  console.log("First Number is greater than second Number" + firstNum>secondNum));
+  console.log(numVar);
+  firstNum = 11;
+  secondNum = 12;
+  console.log("First Number is greater than second Number" + firstNum>secondNum));
+  console.log("First Number: "+firstNum+" second Number: "+secondNum);
+  numVar=numVar+3;
+
+
+}
+comparingNumbers(2,1);
+numVar = numVar + 40;//68
+comparingNumbers(12,18);
+//console.log(firstNum);
