c9fa5bbb7913202a35066aea7603ae3067d531f1
snmhaughton <77122583+snmhaughton@users.noreply.github.com>
2021-02-16 23:13:35 -0800

    lab 10 changes

--- lab10/js/lab10.js
+++ lab10/js/lab10.js
@@ -4,12 +4,27 @@
  * License: Public Domain
  */
 
+//reorder userName taken from lab 7
+function reorderUserName(word){
+  return word.toLowerCase().split('').sort().join('');
+}
 
- //find the button element
+//find the button element
  buttonEl = document.getElementById("my-button");
- //Attach an event listener to your button that gets value from input and runs through sort
+ console.log("button element:", buttonEl);
+//find the form element
+inputEl = document.getElementById("user-name");
+console.log("input element:", inputEl);
+//find output element
+outputEl = document.getElementById("output");
+console.log("output element:", outputEl);
+//Attach an event listener to your button that gets value from input and runs through sort
  buttonEl.addEventListener("click", function(){
-     inputValue=document.getElementById("user-name").value;
-     console.log("you input:", inputValue);
+   //get value from name element
+   var userName = inputEl.value;
+   //modify value  - sort
+   var newName = reorderUserName(userName);
+   //put value in output element
+   outputEl.innerHTML = "<p class=name><span>" + newName + "</span></p>";
 
  });
