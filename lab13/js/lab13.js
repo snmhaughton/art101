6d2f662de54cbcb0ec5b984e0ad98d3318f5d7bb
snmhaughton <77122583+snmhaughton@users.noreply.github.com>
2021-03-01 19:09:41 -0800

    lab 14

--- lab13/js/lab13.js
+++ lab13/js/lab13.js
@@ -4,23 +4,33 @@
  * License: Public Domain
  */
 
- var oneLongString += num + " Fizz!<br>";
-
-for (var num=1; num<=200; num++) {
-  console.log(num);
-  if (num%15 == 0){
-    console.log("FizzBuzz");
-  } if (num%21 == 0){
-    console.log("FizzBoom");
-  } if (num%35 == 0){
-    console.log("BuzzBoom");
-  } if (num%3 == 0);{
-    console.log("Fizz");
-  } if (num%5 == 0);{
-    console.log("Buzz");
-  } if (num%7 == 0);{
-    console.log("Boom");
-  }
-}
+var oneLongString =num;
+  for (var num=1; num <= 200; num++){
+//if number is multiple of all, it should combine FizzBuzzBoom
+    if (num % 105 == 0){
+      console.log("FizzBuzzBoom");
+      oneLongString += num + " - FizzBuzzBoom! <br>";
+    } else if (num%15 == 0){
+      console.log("FizzBuzz");
+      oneLongString += num + " - FizzBuzz! <br>";
+    } else if (num%21 == 0){
+      console.log("FizzBoom");
+      oneLongString += num + " - FizzBoom! <br>";
+    } else if (num%35 == 0){
+      console.log("BuzzBoom");
+      oneLongString += num + " - BuzzBoom! <br>";
+    } else if (num%3 == 0);{
+      console.log("Fizz");
+      oneLongString += num + " - Fizz! <br>";
+    } else if (num%5 == 0);{
+      console.log("Buzz");
+      oneLongString += num + " - Buzz! <br>";
+    } else if (num%7 == 0);{
+      console.log("Boom");
+      oneLongString += num + " - Boom! <br>";
+    } else {console.log(num)
+          oneLongString += num + "<br>";
+    }
+};
 
 $("#output").html(oneLongString);
